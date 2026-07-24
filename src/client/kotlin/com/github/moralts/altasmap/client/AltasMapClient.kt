ddac055fc91c5d1dbe8b701b4791ddaedab39cd0
Huparon <huparon@outlook.com>
2026-07-24 23:36:33 +0800

    feat: first commit

---
+++ src/client/kotlin/com/github/moralts/altasmap/client/AltasMapClient.kt
@@ -0,0 +1,9 @@
+package com.github.moralts.altasmap.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object AltasMapClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
